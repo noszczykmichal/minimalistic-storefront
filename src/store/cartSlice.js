e9bc44d56bde23b592536ac0f487e8e53240dfd3
Michal <noszczykmichal@wp.pl>
2022-11-22 16:16:27 +0100

    more changes in the cartSlice

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -8,62 +8,57 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     addProductToCart(state, action) {
-      // const cart = [...state.cart];
-      // let updatedCart = [];
-      // const incomingProduct = JSON.parse(JSON.stringify(action.payload));
-      // // console.log("incoming: ", incomingProduct);
-      // if (cart.length > 0) {
-      //   /* checking whether the product with given id is already in the cart */
-      //   const filteredCart = cart.filter(
-      //     (product) => product.id === incomingProduct.id,
-      //   );
+      const cart = [...state.cart];
+      let updatedCart = [];
+      const incomingProduct = JSON.parse(JSON.stringify(action.payload));
+      // console.log("incoming: ", incomingProduct);
+      if (cart.length > 0) {
+        /* checking whether the product with given id is already in the cart */
+        const filteredCart = cart.filter(
+          (product) => product.id === incomingProduct.id,
+        );
 
-      //   if (filteredCart.length > 0) {
-      //     filteredCart.map((product) => {
-      //       product.attributes.map((attribute, index) => {
-      //         const incomProdAttribute = incomingProduct.attributes[index];
+        if (filteredCart.length > 0) {
+          const isEqual = filteredCart.map((product) => {
+            return product.attributes.map((attribute, index) => {
+              const incomProdSingleAttribute =
+                incomingProduct.attributes[index];
 
-      //         attribute.items.every((attributeItem, index, array) => {
-      //           const val = incomProdAttribute.items.some(
-      //             (incomingProdOption, index) =>
-      //               console.log(
-      //                 "incomingProdOption: ",
-      //                 incomProdAttribute.name,
-      //                 incomingProdOption,
-      //               ),
-      //             console.log(
-      //               "attributeItem: ",
-      //               attribute.name,
-      //               current(attributeItem),
-      //             ),
-      //           );
+              return attribute.items.every((attributeItem) => {
+                return incomProdSingleAttribute.items.some(
+                  (incomingProdOption) => {
+                    console.log(
+                      "[JSON.stringify(incomingProdOption)] :",
+                      JSON.stringify(incomingProdOption),
+                      "[ JSON.stringify(attributeItem)]: ",
+                      JSON.stringify(attributeItem),
+                    );
 
-      //           console.log(val);
-      //           return attributeItem;
-      //         });
+                    return (
+                      JSON.stringify(incomingProdOption) ===
+                      JSON.stringify(attributeItem)
+                    );
+                  },
+                );
+              });
+            });
+          });
+          console.log("isEqual: ", isEqual);
+          updatedCart = [...cart];
+        } else {
+          /* cart is not empty, no other products with given id; product can be added without attributes checking */
+          incomingProduct.quantity = 1;
+          updatedCart = [...cart];
+          updatedCart.push(incomingProduct);
+        }
+      } else {
+        /* cart empty, a new product can be safely added to the cart */
+        incomingProduct.quantity = 1;
+        updatedCart.push(incomingProduct);
+      }
 
-      //         return attribute;
-      //       });
+      return { ...state, cart: updatedCart, productsTotal: updatedCart.length };
 
-      //       return product;
-      //     });
-
-      //     updatedCart = [...cart];
-      //   } else {
-      //     /* cart is not empty, no other products with given id; product can be added without attributes checking */
-      //     incomingProduct.quantity = 1;
-      //     updatedCart = [...cart];
-      //     updatedCart.push(incomingProduct);
-      //   }
-      // } else {
-      //   /* cart empty, a new product can be safely added to the cart */
-      //   incomingProduct.quantity = 1;
-      //   updatedCart.push(incomingProduct);
-      // }
-
-      // return { ...state, cart: updatedCart, productsTotal: updatedCart.length };
-
-      return state;
       // const updatedCart = [...state.cart].concat(action.payload);
       // const updatedProductsTotal = updatedCart.length;
       // return {
