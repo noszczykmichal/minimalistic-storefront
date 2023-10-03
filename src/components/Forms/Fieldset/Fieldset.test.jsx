956384377654414ffe61220e1b5f6a94457cd32a
Michal <noszczykmichal@wp.pl>
2023-10-04 00:07:57 +0200

    added tests for the Fieldset component

---
+++ src/components/Forms/Fieldset/Fieldset.test.jsx
@@ -0,0 +1,152 @@
+jest.mock("../../../hooks/useReduxHooks.ts", () => ({
+  ...jest.requireActual("../../../hooks/useReduxHooks.ts"),
+  useAppDispatch: jest.fn(),
+}));
+
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import configureStore from "redux-mock-store";
+import { Provider } from "react-redux";
+import userEvent from "@testing-library/user-event";
+
+import Fieldset from "./Fieldset";
+import { useAppDispatch } from "../../../hooks/useReduxHooks";
+import { shippingPaymentOptionsActions } from "../../../store/shippingPaymentOptions";
+
+const testOptions = [
+  {
+    label: "Flat Rate",
+    name: "flatRate",
+    costs: [
+      { amount: 5.0, currency: { label: "USD", symbol: "$" } },
+      { amount: 3.59, currency: { label: "GBP", symbol: "£" } },
+    ],
+  },
+  {
+    label: "Best Way",
+    name: "bestWay",
+    costs: [
+      { amount: 10.0, currency: { label: "USD", symbol: "$" } },
+      { amount: 7.19, currency: { label: "GBP", symbol: "£" } },
+    ],
+  },
+];
+
+const mockStore = configureStore([]);
+
+describe("Fieldset component", () => {
+  const dispatch = jest.fn();
+  const { registerOption, optionChangeHandler } = shippingPaymentOptionsActions;
+  const fieldIdentifier = "shippingOption";
+  let store;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.restoreAllMocks();
+    useAppDispatch.mockReturnValue(dispatch);
+
+    store = mockStore({
+      products: { billingCurrency: "$" },
+      shippingPaymentOptions: {
+        inputs: { shippingOption: { value: "flatRate" } },
+      },
+    });
+  });
+
+  it("should render a fieldset with 2 inputs on the basis of provided data", () => {
+    render(
+      <Provider store={store}>
+        <Fieldset
+          options={testOptions}
+          heading="Delivery"
+          identifier={fieldIdentifier}
+        />
+      </Provider>,
+    );
+
+    const fieldsetElem = screen.getByRole("group");
+    const radioElements = screen.getAllByRole("radio");
+
+    expect(fieldsetElem).toBeInTheDocument();
+    expect(radioElements).toHaveLength(testOptions.length);
+  });
+
+  it("should dispatch an action on initial render and register a new fieldset", () => {
+    const newStore = mockStore({
+      products: { billingCurrency: "$" },
+      shippingPaymentOptions: {
+        inputs: {},
+      },
+    });
+
+    render(
+      <Provider store={newStore}>
+        <Fieldset
+          options={testOptions}
+          heading="Delivery"
+          identifier={fieldIdentifier}
+        />
+      </Provider>,
+    );
+
+    expect(dispatch).toHaveBeenCalledTimes(1);
+    expect(dispatch).toHaveBeenLastCalledWith(registerOption(fieldIdentifier));
+  });
+
+  it("should update the state if a value for given fieldset is registered in the store", () => {
+    const mockedSetCheckedInputName = jest.fn();
+    const mockedCheckedInputName = "";
+    const mockedValue = "flatRate";
+
+    jest
+      .spyOn(React, "useState")
+      .mockReturnValueOnce([mockedCheckedInputName, mockedSetCheckedInputName]);
+
+    render(
+      <Provider store={store}>
+        <Fieldset
+          options={testOptions}
+          heading="Delivery"
+          identifier={fieldIdentifier}
+        />
+      </Provider>,
+    );
+
+    expect(mockedSetCheckedInputName).toHaveBeenCalledTimes(1);
+    expect(mockedSetCheckedInputName).toHaveBeenCalledWith(mockedValue);
+  });
+
+  it("should call clickHandler when clicked and dispatch an action", () => {
+    const { costs } = testOptions[1];
+    const billingCurrency = "$";
+    const optionPrice = costs.find(
+      (cost) => cost.currency.symbol === billingCurrency,
+    ).amount;
+
+    render(
+      <Provider store={store}>
+        <Fieldset
+          options={testOptions}
+          heading="Delivery"
+          identifier={fieldIdentifier}
+        />
+      </Provider>,
+    );
+
+    const radioElement = screen.getByLabelText(
+      new RegExp(`\\${testOptions[1].label}`),
+    );
+
+    userEvent.click(radioElement);
+
+    expect(dispatch).toHaveBeenCalledTimes(2);
+    expect(dispatch).toHaveBeenNthCalledWith(
+      2,
+      optionChangeHandler({
+        identifier: fieldIdentifier,
+        name: radioElement.getAttribute("name"),
+        optionCost: optionPrice,
+      }),
+    );
+  });
+});
